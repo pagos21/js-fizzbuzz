d8c305d2e88cfdb27eee0594fbd48388359bfe45
pagos21 <paga@localhost.yeah>
2020-06-26 09:45:58 +0200

    bonus + setInterval

--- BONUS/JS/script.js
+++ BONUS/JS/script.js
@@ -1,78 +1,9 @@
-// Tentativo di esecuzione con setTimeout
+// Tentativo di esecuzione con setTinterval
 
 btn.addEventListener("click", function() {
 
-  // arr = [];
-  //
-  // for (var i = 1; i < 101; i++) {
-  //   arr.push(i);
-  // }
-  //
-  // function delay() {
-  //     console.log(arr[c]);
-  //   }
-  //
-  // }
-  //
-  // setTimeout(delay(), 1000);
-
-
-
-
-
-
-
-  // var printNumbersForEvery02Sec = (n) => {
-  //
-  //   loop0: for (let i = 1; i <= n; i++) {
-  //
-  //     setTimeout(function(){
-  //       if (i%3 == 0 && i%5 == 0) {
-  //        console.log("FizzBuzz");
-  //        // continue loop0;
-  //      }
-  //       else if (i%3 == 0) {
-  //         console.log("Fizz");
-  //         // continue loop0;
-  //
-  //       }
-  //       else if (i%5 == 0) {
-  //         console.log("Buzz");
-  //         // continue loop0;
-  //       }
-  //       console.log(i)
-  //     }, i * 250);
-  //   }
-  // }
-  // printNumbersForEvery02Sec(100);
-
-
-  // for (var i = 1; (i < 100 +1); i++) {
-  //
-  //   if (i%3 == 0 && i%5 == 0) {
-  //    console.log("FizzBuzz");
-  //    setInterval(function(){}, 250)
-  //    continue;
-  //  }
-  //   else if (i%3 == 0) {
-  //     console.log("Fizz");
-  //     setInterval(function(){}, 250)
-  //     continue;
-  //   }
-  //   else if (i%5 == 0) {
-  //     console.log("Buzz");
-  //     setInterval(function(){}, 250)
-  //     continue;
-  //   }
-  //   setInterval(function(){}, 250)
-  //
-  //   console.log(i);
-  // }
-
-
   var i = 1;
   var delay = setInterval(function(){
-    console.log(i);
 
       if (i%3 == 0 && i%5 == 0) {
        console.log("FizzBuzz");
@@ -85,8 +16,11 @@ btn.addEventListener("click", function() {
       else if (i%5 == 0) {
         console.log("Buzz");
       }
+      else {
+        console.log(i);
+      }
 
-       if(i === 10){
+       if(i === 101){
          clearInterval(delay);
         }
         i++;
